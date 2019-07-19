0c18c9918d3e05d6fa2ed5edad60f0bc6962bcfb
PedroFurlan <PedroHenriqueFurlanSantos>
2019-07-19 17:14:28 -0300

    subir atividades do club e emprestimo

--- script.js
+++ script.js
@@ -1,54 +1,27 @@
-let nome = (prompt("Digite seu nome"));
-console.log("Seu nome é:" + nome)
-
-let idade = Number(prompt('digite sua idade'));
-console.log('sua idade é :' + idade)
-
-if (idade >= 60) {
-    console.log("sua entrada e gratuida")
+let dia = Number(prompt("Digite um valor e eu lhe respondereiqual qual e o dia da semana correspondente"));
+if (dia == 1) {
+    alert ("Domingo");
 }
-else if (idade >= 0 && idade <= 12) {
-    console.log("Vocé paga $12.00.")
+else if (dia == 2) {
+    alert ("segunda-feira");
 }
-else if (idade >= 13 && idade <= 17) {
-    console.log("Vocé paga $15.00.")
+else if (dia == 3) {
+    alert ("terça-feira")
 }
-else if (idade >= 18 && idade <= 59) {
-    let estu = window.confirm("Vocé é estudande")
-    if (estu) {
-        alert("Vocé paga meia R$17,50")
-    }
-    else {
-        alert("Vocé não é estudate então paga R$35,00")
-    }
+else if (dia == 4){
+    alert("quarta-feira")
+} 
+else if (dia == 5){
+    alert("quinta-feira")
+}
+else if (dia == 6){
+alert("sexta-feira")
+}
+else if (dia == 7){
+    alert("sabádo")
+} 
+else {
+    alert("número inválido.")
 }
 
-// let nome =(prompt(`Digite seu nome ,`));{
-//     confirm("Este e seu nome " + nome + "?");
-    
-//     }
-//     let idade = Number(prompt(`Digite sua idade`));{
-//         confirm(" Voce tem " + idade + " anos,");
-    
-//     }
-//     if (idade >= 25 && idade <=60) {
-// console.log ("sua idade foi aprovada .");
-//     }
-    
-    
-//     else{
-//         console.log("sua Idade foi reprovada . ")
-//     }
-//     let salario = Number(prompt("Digite seu salario ."));{
-//         confirm("Seu salario é " + salario + "?");
 
-//     }
-//     if (salario>=2000 ){
-//         console.log("Seu salario foi aprovado .")
-
-//     }
-
-//     else{
-//         console.log("Seu salrio foi aprovado .");
-//     }
-//     console.log("Voce " + nome + ", com o salario de " + salario + ",pode  pedir emprestimo no maximo de " +(salario*10))
